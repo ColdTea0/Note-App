325164fb165c99f66ceea95f6a9c25cf777520ef
Cold-Tea <ezralawrence05@gmail.com>
2025-06-08 13:01:11 -0500

    first commit

---
+++ server/server.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded(extended:true))
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT})`);
+});
